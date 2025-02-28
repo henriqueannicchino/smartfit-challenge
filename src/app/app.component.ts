a78ef5efa0b74ae3bcc48c956d0e84805c8d9bf3
Luis Henrique <42474086+henriqueannicchino@users.noreply.github.com>
2025-02-28 18:06:07 +0100

    feat: added cards and caption

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { FormsComponent } from "./components/forms/forms.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { BehaviorSubject } from 'rxjs';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { CommonModule } from '@angular/common';
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
+import { CaptionComponent } from "./components/caption/caption.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeaderComponent, FormsComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeaderComponent,
+    FormsComponent,
+    CardsListComponent,
+    CommonModule,
+    CaptionComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'smartfit-challenge';
+  showList = new BehaviorSubject(false);
+  unitsList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService){}
+
+  onSubmit() {
+    this.showList.next(true);
+    this.unitsList = this.unitService.getFilteredUnits();
+  }
 }
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
@@ -12,6 +12,7 @@ import { FilterUnitsService } from '../../services/filter-units.service';
   styleUrl: './forms.component.scss',
 })
 export class FormsComponent implements OnInit {
+  @Output() submitEvent = new EventEmitter();
   results: Location[] = [];
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
@@ -41,6 +42,8 @@ export class FormsComponent implements OnInit {
       hour
     );
     this.unitService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
   }
 
   onClean(): void {
